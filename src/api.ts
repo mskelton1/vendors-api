a2cd2f4ac1262c8457fedbbb78a6744c8f78e65f
mskelton1 <mskelton1@dwd.in.gov>
2025-11-17 15:49:04 -0500

    api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,13 @@
+import { getField, apiKey } from "./internal";
 import type { VendorsBehavioursApi } from "./types";
-import { apiKey, getField } from "./internal";
 
-/**
- * Initialize ScriptRunner Vendors API Behaviours bridge to custom field allowing Jira
- * administrators to use Groovy scripts to manipulate the field state in Issue Creation
- * from.
- *
- * @param {string} customFiledId
- * @param {VendorsBehavioursApi<T>} apiSpec
- */
 export const init = <T>(
-  customFiledId: string,
+  customFieldId: string,
   apiSpec: VendorsBehavioursApi<T>,
-) => {
-  const field = getField(customFiledId);
-  field.data(apiKey, apiSpec);
-  field.attr(`data-${apiKey}`, "true");
+): void => {
+  const field = getField(customFieldId);
+  if (field) {
+    field.data(apiKey, apiSpec);
+    field.attr(`data-${apiKey}`, "true");
+  }
 };
